f21c55eebeaabf5c29f80ab4d359738e2c1c2592
Aldair <aldairgarcia803@gmail.com>
2019-02-17 17:43:17 +0000

    new

---
+++ in.js
@@ -0,0 +1,42 @@
+/*global $*/
+$("#two").click(function(){
+
+	var english= $("#one").val() ;
+	
+	var piglat= english.split(" ");
+	
+	$("#four").html(piglat);
+	
+	for( var i=0; i < piglat.length ; i++){
+		var first = piglat.charAt(0);
+	
+		var wordtwo = piglat.slice(1);
+	
+			if(first==="a"){
+			$("#five").html(piglat[i]+"ay");
+		} 
+		else if(first==="e"){
+			$("#five").html(piglat[i]+"ay");
+		}
+		else if(first==="i"){
+			$("#five").html(piglat[i]+"ay");
+		} 
+			else if(first==="o"){
+			$("#five").html(piglat[i]+"ay");
+		} 
+		else if(first=== "u"){
+			$("#five").html(piglat[i]+"ay");
+			}
+		else if(first!== "a"){
+			$("#five").html(wordtwo+first+"ay");
+		}	
+		
+		$("#three").append( piglat[i] +"ay ");
+		console.log(piglat[i]);
+	
+	}
+	$("#te").html(first);
+	$(".te").html(wordtwo);
+});
+
+
